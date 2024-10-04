1a15df26b9376816e20e0de860d1cbec2ccf7846
sachin-rai-dev <sachinrai6782000@gmail.com>
2024-10-04 16:03:21 +0530

    ty

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore} from 'firebase/firestore';
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey:`${process.env.REACT_APP_APIKEY}`,
